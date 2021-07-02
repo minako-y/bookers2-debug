9114e401f632bdaa454a5dc5e93fad731040719b
minako-y <m.tp2uk@gmail.com>
2021-07-02 02:25:37 +0000

    [Finish]デバッグの終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,17 +2,19 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @new_book = Book.new
   end
 
   def index
     @books = Book.all
+    @new_book = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+    if @new_book.save
+      redirect_to book_path(@new_book), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
@@ -21,9 +23,10 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
   end
-
-
 
   def update
     @book = Book.find(params[:id])
@@ -34,16 +37,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
-    @book.destoy
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
